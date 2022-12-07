14ae35b1eea4a56aea6abd1b1a6f95eb31191a2a
tsadahiro <sadahiro@tsuda.ac.jp>
2022-12-07 18:22:27 +0900

    4

--- 4/matrix.r
+++ 4/matrix.r
@@ -1,10 +1,12 @@
 A = matrix(c(1,2,3,4,
              5,6,7,8,
              9,10,11,12),
-           ncol=3,
+           ncol=4,
            byrow=T)
 
 Id = diag(rep(1,4)) #単位行列
 
-A = rbind(A,Id) #Aの下に単位行列を結合する。           
+A = rbind(A,Id) #Aの下に単位行列を結合する。
+
+A
            
