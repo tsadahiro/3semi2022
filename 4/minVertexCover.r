d55eaccd697a4d6a8a3dc5580966030aa37c7d13
tsadahiro <sadahiro@tsuda.ac.jp>
2022-12-07 11:36:48 +0900

    4

--- 4/minVertexCover.r
+++ 4/minVertexCover.r
@@ -2,7 +2,7 @@ library(igraph)
 library(lpSolve)
 
 #G = make_lattice(c(3,3,3))
-G = erdos.renyi.game(40,0.1)
+G = erdos.renyi.game(40,0.05)
 layout = layout.kamada.kawai(G,dim=2)
 plot(G, layout=layout)
 #rglplot(G,layout=layout.kamada.kawai(G,dim=3))
@@ -15,17 +15,25 @@ A = t(apply(ends(G,E(G)), 1, function(x){v=rep(0, n);v[x]=1;v}))
 dir = rep(">=",m)
 b = rep(1,m)
 
-sol=lp(direction="min",
+optSol=lp(direction="min",
        objective.in=c,
        const.mat=A,
        const.dir=dir,
        const.rhs=b,
        int.vec=1:n)
 
+apprSol=lp(direction="min",
+       objective.in=c,
+       const.mat=A,
+       const.dir=dir,
+       const.rhs=b)
+
 pal=c("pink","cyan")
-#rglplot(G,
-#        layout=layout.kamada.kawai(G,dim=3),
-#        vertex.color=pal[round(sol$solution)+1])
+
 plot(G,
      layout=layout,
-     vertex.color=pal[round(sol$solution)+1])
+     vertex.color=pal[round(optSol$solution)+1])
+
+plot(G,
+     layout=layout,
+     vertex.color=pal[as.numeric(apprSol$solution>=0.5)+1])
